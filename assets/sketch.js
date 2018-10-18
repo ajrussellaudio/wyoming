e13c216d4b503f8f8f202661241f4637ff46ea66
Alan Russell <ajrussellaudio@gmail.com>
2018-10-18 17:26:59 +0100

    add mountains

--- assets/sketch.js
+++ assets/sketch.js
@@ -1,15 +1,66 @@
+let mountains;
+let sky;
+
+// foreground colours: #570335, #33152D
+
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
-  console.log("p5 loaded!");
+  noStroke();
+  const colours = ["#FF864F", "#FF7344", "#FB463E", "#C9253E", "#830034"];
+  mountains = [];
+  sky = new Sky("#FFDCC5");
+  sky.draw();
+  for (let i = 0; i < colours.length; i++) {
+    mountains.push(new MountainsLayer(colours[i], i, colours.length));
+  }
+  mountains.forEach(mountain => mountain.draw());
 }
 
-function draw() {
-  background("#EB285F");
-  stroke(255);
-  for (let i = 0; i < 80; i++) {
-    strokeWeight(int(random(20)));
-    const x = randomGaussian(width / 2, width / 8);
-    const y = randomGaussian(height / 2, height / 8);
-    point(x, y);
+class MountainsLayer {
+  constructor(colour, depth, count) {
+    this.colour = colour;
+    this.depth = depth + 1;
+    this.count = count;
+    const numPoints = int(
+      random((this.count * 10) / this.depth, (this.count * 40) / this.depth)
+    );
+    this.points = [];
+    noiseSeed(random(138));
+    let offset = random();
+    for (let i = 0; i < numPoints; i++) {
+      offset += 0.1;
+      this.points.push(noise(offset));
+    }
+  }
+
+  draw() {
+    fill(this.colour);
+    beginShape();
+    vertex(0, height);
+    const distance = map(
+      this.depth,
+      0,
+      this.count,
+      height / 6,
+      (height * 2) / 3
+    );
+    for (let i = 0; i < this.points.length; i++) {
+      const x = map(i, 0, this.points.length - 1, 0, width);
+      const y = map(this.points[i], 0, 1, 0, 200) + distance;
+      vertex(x, y);
+    }
+    vertex(width, height);
+    endShape();
+  }
+}
+
+class Sky {
+  constructor(colour) {
+    this.colour = colour;
+  }
+
+  draw() {
+    fill(this.colour);
+    rect(0, 0, width, height);
   }
 }
